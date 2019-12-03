225b94ea50b16c7f1937c1278875085dbdb3b2ba
Ana Cecília Souza <ana.souza@topos.local>
2019-12-03 11:20:47 -0300

    First Commit

---
+++ QATest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QATest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QATest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c18109a-de2c-4f83-9c35-70da856f7427")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QATest/Steps/AmazonFeatureSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+
+namespace QATest.Steps
+{
+    [Binding]
+    public class AmazonFeatureSteps
+    {
+        private IWebDriver webDrive;
+
+        [Given(@"I navigate to “www.amazon.com”.")]
+        public void NavigateToAmazon()
+        {
+            webDrive.Navigate().GoToUrl("http://www.amazon.com");
+        }
+
+        [When(@"I select the option “Books” in the dropdown next to the search text input criteria\.")]
+        public void SelectSearchOption()
+        {
+            searchOption = webDrive.FindElement(By.Id("searchDropdownBox"));
+            searchOption.SelectByValue("Livros");
+        }
+
+        [Then(@"I search for “Test automation”.")]
+        public void SearchText(string text)
+        {
+            searchBox = webDrive.FindElement(By.Id("twotabsearchtextbox"));
+            searchBox.SendKeys(text);
+            searchBox.Submit();
+        }
+
+        [Then(@"I select the cheapest book of the page without using any sorting method available\.")]
+        public void SelectCheapestBook()
+        {
+            ScenarioContext.Current.Pending(); //INSERIR A LOGICA
+        }
+
+        [When(@"I reach the detailed book page, I check if the name in the header is the same name of the book that I select previously\.")]
+        public void VerifyHeader()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
